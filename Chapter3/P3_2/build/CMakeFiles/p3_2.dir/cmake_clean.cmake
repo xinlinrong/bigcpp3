4aa3d3f47c2ba9b8433c4132eaa1efc7fe413384
549716158@qq.com <549716158@qq.com>
2020-05-11 00:49:59 +0800

    [bigcpp_my_solution] add P3_2

---
+++ Chapter3/P3_2/build/CMakeFiles/p3_2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/p3_2.dir/main.cpp.o"
+  "p3_2.pdb"
+  "p3_2"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/p3_2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
